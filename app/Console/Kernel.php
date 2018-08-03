812192bcbab3c748bfd7ecb1ae5898f9d091ab89
Yann Labour <yann@sugarsplashes.com>
2018-08-03 11:50:26 +0400

    parse website more frequently

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('parse:website')->hourly();
+        $schedule->command('parse:website')->everyFiveMinutes();
         $schedule->command('queue:work --tries=3')
             ->everyMinute()
             ->withoutOverlapping();
